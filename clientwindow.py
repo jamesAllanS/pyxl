ab2b3e2afda6ff78bb563102a256d39bc596ab9f
jamesAllanS <zy319796@ou.ac.uk>
2019-09-30 00:52:27 +0100

    format doc

--- clientwindow.py
+++ clientwindow.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from subprocess import run
 
+
 class ClientWindow:
     def __init__(self, aCompletedProcess):
         self._CLIENT = aCompletedProcess.args[0]
@@ -15,25 +16,24 @@ class ClientWindow:
                'PID: ' + self.PID + ',' + \
                'Window ID: ' + self.WINDOW_ID + '\n'
 
-
     @property
     def WINDOW_ID(self):
         return self._WINDOW_ID
+
     @property
     def CLIENT(self):
         return self._CLIENT
+
     @property
     def PID(self):
         return self._PID
 
-
     def setTitleToClientName(self):
         """Sets the window title to that of the client (no path etc)"""
         self.title = self.CLIENT
-        run(['xdotool', 'set_window', '--name', self.title ], capture_output=True).stdout.decode('utf-8').strip()
-        
+        run(['xdotool', 'set_window', '--name', self.title],
+            capture_output=True).stdout.decode('utf-8').strip()
+
     def getClientPID(self):
         """Returns a client's PID as an int"""
         return self.proc.pid
-
-
